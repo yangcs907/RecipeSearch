472d9e8c027526a6409c09bd709ffe2d94fc2a9e
Chris Yang <chrisyang@Chriss-MacBook-Pro.local>
2018-06-13 19:28:21 -0400

    changes

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use("/api", apiRoutes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//app.get("*", function(req, res) {
+  //res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
